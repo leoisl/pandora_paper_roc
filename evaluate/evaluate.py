2a64184fe73073b962cbde3e7af73054e4f3f732
michael <michael@mbh.sh>
2019-08-09 14:00:15 +0100

    add method to generate a mummer snps file for two sequences

--- evaluate/evaluate.py
+++ evaluate/evaluate.py
@@ -1,15 +1,39 @@
 from evaluate.cli import cli
-from evaluate.mummer import Nucmer
+from evaluate.mummer import Nucmer, DeltaFilter, ShowSnps
 from evaluate.utils import strip_extensions
 from io import StringIO
 from pathlib import Path
-
+import logging
 
 def generate_mummer_snps(
-    reference: Path, query: Path, prefix: Path = Path("out")
+    reference: Path, query: Path, prefix: Path = Path("out"), flank_width: int = 0
 ) -> StringIO:
+    logging.info("Generating MUMmer SNPs file.")
+
     nucmer_params = "--maxmatch"
     nucmer = Nucmer(reference, query, str(prefix), extra_params=nucmer_params)
+    nucmer_result = nucmer.run()
+    nucmer_result.check_returncode()
+
+    deltafile = prefix.with_suffix(".delta")
+    deltafilter_params = "-1"
+    deltafilter = DeltaFilter(deltafile, extra_params=deltafilter_params)
+    deltafilter_result = deltafilter.run()
+    deltafilter_result.check_returncode()
+
+    filtered_deltafile = prefix.with_suffix(".delta1")
+    _ = filtered_deltafile.write_text(deltafilter_result.stdout.decode())
+
+    showsnps_params = "-rlTC"
+    showsnps = ShowSnps(
+        filtered_deltafile, context=flank_width, extra_params=showsnps_params
+    )
+    showsnps_result = showsnps.run()
+    showsnps_result.check_returncode()
+
+    logging.info("Finished generating MUMmer SNPs file.")
+
+    return StringIO(showsnps_result.stdout.decode())
 
 
 def main():
@@ -19,7 +43,7 @@ def main():
     query: Path = args.query2
     query_name: str = strip_extensions(query).name
     prefix: Path = args.temp / f"{reference_name}_{query_name}"
-    mummer_snps = generate_mummer_snps(reference, query, prefix)
+    mummer_snps = generate_mummer_snps(reference, query, prefix, args.truth_flank)
 
 
 if __name__ == "__main__":
--- tests/test_evaluate.py
+++ tests/test_evaluate.py
@@ -1,19 +1,36 @@
 from pathlib import Path
 from evaluate.mummer import NucmerError
 from evaluate.evaluate import generate_mummer_snps
+from io import StringIO
 import pytest
 
-cft = Path("tests/test_cases/CFT073.ref.fa")
+REF = Path("tests/test_cases/ref.fa")
+QUERY = Path("tests/test_cases/query.fa")
+SNPS = Path("tests/test_cases/out.snps")
 
 
 def test_getMummerSnps_invalidQueryFileRaisesNucmerError():
-    reference = cft
+    reference = REF
     query = Path("foo")
     with pytest.raises(NucmerError):
         generate_mummer_snps(reference, query)
 
-def test_getMummerSnps_invalidQueryFileRaisesNucmerError():
-    reference = cft
-    query = Path("foo")
-    with pytest.raises(NucmerError):
-        generate_mummer_snps(reference, query)
+
+def test_getMummerSnps_validSequenceFilesProducesExpectedSnpsFile():
+    reference = REF
+    query = QUERY
+    prefix = Path("/tmp/ref_query")
+    context = 3
+
+    actual = generate_mummer_snps(reference, query, prefix, flank_width=context)
+    expected = StringIO(
+        """/home/michael/Projects/pandora1_paper/tests/test_cases/ref.fa /home/michael/Projects/pandora1_paper/tests/test_cases/query.fa
+NUCMER
+
+[P1]\t[SUB]\t[SUB]\t[P2]\t[BUFF]\t[DIST]\t[LEN R]\t[LEN Q]\t[CTX R]\t[CTX Q]\t[FRM]\t[TAGS]
+39\tG\t.\t38\t34\t38\t85\t84\tGTAGTAG\tGTA.TAG\t1\t1\tref\tquery
+73\tT\tA\t72\t13\t13\t85\t84\tGGATTGA\tGGAATGA\t1\t1\tref\tquery
+"""
+    )
+
+    assert actual.readlines() == expected.readlines()
