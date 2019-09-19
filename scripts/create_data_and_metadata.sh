a9f4bd6a414a198d96435137ba5e0d95ed39a212
leandro <leandro@ebi.ac.uk>
2019-09-19 17:29:58 +0100

    Updating create_data_and_metadata.sh

--- scripts/create_data_and_metadata.sh
+++ scripts/create_data_and_metadata.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 wget https://www.dropbox.com/s/7804udh9c6th7v9/data_and_metadata.zip?dl=1 -O data_and_metadata.zip
-cd .. && unzip Scripts/data_and_metadata.zip
-rm Scripts/data_and_metadata.zip
+cd .. && unzip scripts/data_and_metadata.zip
+rm scripts/data_and_metadata.zip
