8d8170d00234497969d180ec01122b915b6e5f4c
leandro <leandro@ebi.ac.uk>
2019-09-24 13:00:26 +0100

    Adding GapsFilter to get_list_of_all_VCF_Filters

--- evaluate/vcf_filters.py
+++ evaluate/vcf_filters.py
@@ -2,7 +2,7 @@ from .vcf import VCF
 from collections import UserList
 from .coverage_filter import CoverageFilter
 from .strand_bias_filter import StrandBiasFilter
-
+from .gaps_filter import GapsFilter
 
 class VCF_Filters(UserList):
     def record_should_be_filtered_out(self, vcf_record: VCF) -> bool:
@@ -12,9 +12,10 @@ class VCF_Filters(UserList):
 
     @staticmethod
     def get_list_of_all_VCF_Filters(
-        coverage_threshold: float, strand_bias_threshold: float
+        coverage_threshold: float, strand_bias_threshold: float, gaps_threshold: float
     ) -> "VCF_Filters":
         vcf_filters = VCF_Filters()
         vcf_filters.append(CoverageFilter(coverage_threshold))
         vcf_filters.append(StrandBiasFilter(strand_bias_threshold))
+        vcf_filters.append(GapsFilter(gaps_threshold))
         return vcf_filters
