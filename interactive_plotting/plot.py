7b175bfdd4424feb42aaad29f768ca9419922497
leandro <leandro@ebi.ac.uk>
2019-11-23 17:36:43 +0000

    Putting the plot and plot_on_the_web in a single script

--- interactive_plotting/plot.py
+++ interactive_plotting/plot.py
@@ -5,9 +5,6 @@ import pandas as pd
 import plotly.graph_objs as go
 from dash.dependencies import Input, Output
 
-# the only thing that needs to be configured
-ROC_data_path = "ROC_data_gt_min_0.0_step_1.0_max_1000000000.0.tsv"
-
 
 def get_div_for_the_filters(filter_name, filter_values):
     return html.Div([html.H4(f"{filter_name} filters:"),
@@ -22,7 +19,7 @@ def get_div_for_the_filters(filter_name, filter_values):
                                      "margin-right": "auto"})
 
 
-def create_interactive_visualisation():
+def create_interactive_visualisation_app(ROC_data_path):
     # load df
     df = pd.read_csv(ROC_data_path, sep="\t")
 
@@ -33,9 +30,11 @@ def create_interactive_visualisation():
     strand_bias_filters = df["strand_bias_threshold"].unique()
     gaps_filters = df["gaps_threshold"].unique()
 
-    external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-    dash_app = dash.Dash("ROC_pandora", external_stylesheets=external_stylesheets)
+    # set up the app
+    external_stylesheets = ["css/style.css"]
+    dash_app = dash.Dash("ROC_pandora", assets_folder="assets", external_stylesheets=external_stylesheets)
 
+    # set up the layout
     dash_app.layout = html.Div([html.Div([html.H1("Pandora ROC evaluation")], style={'textAlign': "center"}),
                           get_div_for_the_filters("Tool", tools),
                           get_div_for_the_filters("Dataset coverage", dataset_coverages),
@@ -45,7 +44,7 @@ def create_interactive_visualisation():
                           html.Div([dcc.Graph(id="my-graph", style={'height': '1000px', 'width': '1000px'})]),
                           ], className="container")
 
-
+    # register the update callback
     @dash_app.callback(
         Output('my-graph', 'figure'),
         [Input(component_id='Tool_checklist', component_property='value'),
@@ -61,8 +60,7 @@ def create_interactive_visualisation():
                     for strand_bias_threshold in strand_bias_checklist_values:
                         for gaps_threshold in gaps_checklist_values:
                             df_for_label = df.query(f"tool==\"{tool}\" & coverage==\"{dataset_coverage}\" & coverage_threshold=={coverage_threshold} & strand_bias_threshold=={strand_bias_threshold} & gaps_threshold=={gaps_threshold}")
-                            trace_name = f"tool={tool}_coverage={dataset_coverage}_coverage_threshold={coverage_threshold}_strand_bias_threshold={strand_bias_threshold}_gaps_threshold={gaps_threshold}"
-
+                            trace_name = f"Tool: {tool}, Coverage: {dataset_coverage}, Coverage threshold: {coverage_threshold}, Strand bias threshold: {strand_bias_threshold}, Gaps threshold: {gaps_threshold}"
                             trace.append(go.Scatter(x=df_for_label["error_rate"], y=df_for_label["recall"], name=trace_name, mode='lines',
                                                     marker={'size': 8, "opacity": 0.6, "line": {'width': 0.5}}, ))
 
@@ -71,9 +69,9 @@ def create_interactive_visualisation():
                                     yaxis={"title": "Recall"}, xaxis={"title": "Error rate"},
                                     legend_orientation="h")}
 
-
-    dash_app.run_server(debug=True)
+    return dash_app
 
 
 if __name__ == '__main__':
-    create_interactive_visualisation()
+    dash_app = create_interactive_visualisation_app(ROC_data_path ="ROC_data_gt_min_0.0_step_1.0_max_1000000000.0.tsv")
+    dash_app.run_server(debug=True)
--- interactive_plotting/pythonanywhere/flask_app.py
+++
@@ -1,2 +0,0 @@
-import plot_on_web
-app = plot_on_web.create_interactive_visualisation()
