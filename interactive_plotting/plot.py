937d9da2e531daff516db51738b14739af8ccc07
leandro <leandro@ebi.ac.uk>
2019-11-25 13:39:19 +0000

    Updating interactive plotting

--- interactive_plotting/plot.py
+++ interactive_plotting/plot.py
@@ -19,6 +19,25 @@ def get_div_for_the_filters(filter_name, filter_values):
                                      "margin-right": "auto"})
 
 
+def compute_all_possible_traces(df, tools, dataset_coverages, coverage_filters, strand_bias_filters, gaps_filters):
+    config_to_trace = {}
+    for tool in tools:
+        for dataset_coverage in dataset_coverages:
+            for coverage_threshold in coverage_filters:
+                for strand_bias_threshold in strand_bias_filters:
+                    for gaps_threshold in gaps_filters:
+                        df_for_label = df.query(
+                            f"tool==\"{tool}\" & coverage==\"{dataset_coverage}\" & coverage_threshold=={coverage_threshold} & strand_bias_threshold=={strand_bias_threshold} & gaps_threshold=={gaps_threshold}")
+
+                        trace_name = f"Tool: {tool}, Coverage: {dataset_coverage}, Coverage threshold: {coverage_threshold}, Strand bias threshold: {strand_bias_threshold}, Gaps threshold: {gaps_threshold}"
+                        trace = go.Scatter(x=df_for_label["error_rate"], y=df_for_label["recall"], name=trace_name,
+                                                mode='lines',
+                                                marker={'size': 8, "opacity": 0.6, "line": {'width': 0.5}})
+
+                        config_to_trace[(tool, dataset_coverage, coverage_threshold, strand_bias_threshold, gaps_threshold)] = trace
+    return config_to_trace
+
+
 def create_interactive_visualisation_app(ROC_data_path):
     # load df
     df = pd.read_csv(ROC_data_path, sep="\t")
@@ -44,6 +63,10 @@ def create_interactive_visualisation_app(ROC_data_path):
                           html.Div([dcc.Graph(id="my-graph", style={'height': '1000px', 'width': '1000px'})]),
                           ], className="container")
 
+    # pre-compute all traces
+    config_to_all_traces = compute_all_possible_traces(df, tools, dataset_coverages, coverage_filters, strand_bias_filters, gaps_filters)
+
+
     # register the update callback
     @dash_app.callback(
         Output('my-graph', 'figure'),
@@ -53,18 +76,15 @@ def create_interactive_visualisation_app(ROC_data_path):
          Input(component_id='Strand bias_checklist', component_property='value'),
          Input(component_id='Gaps_checklist', component_property='value')])
     def update_figure(tool_checklist_values, dataset_coverage_checklist_values, coverage_checklist_values, strand_bias_checklist_values, gaps_checklist_values):
-        trace = []
+        traces = []
         for tool in tool_checklist_values:
             for dataset_coverage in dataset_coverage_checklist_values:
                 for coverage_threshold in coverage_checklist_values:
                     for strand_bias_threshold in strand_bias_checklist_values:
                         for gaps_threshold in gaps_checklist_values:
-                            df_for_label = df.query(f"tool==\"{tool}\" & coverage==\"{dataset_coverage}\" & coverage_threshold=={coverage_threshold} & strand_bias_threshold=={strand_bias_threshold} & gaps_threshold=={gaps_threshold}")
-                            trace_name = f"Tool: {tool}, Coverage: {dataset_coverage}, Coverage threshold: {coverage_threshold}, Strand bias threshold: {strand_bias_threshold}, Gaps threshold: {gaps_threshold}"
-                            trace.append(go.Scatter(x=df_for_label["error_rate"], y=df_for_label["recall"], name=trace_name, mode='lines',
-                                                    marker={'size': 8, "opacity": 0.6, "line": {'width': 0.5}}, ))
+                            traces.append(config_to_all_traces[(tool, dataset_coverage, coverage_threshold, strand_bias_threshold, gaps_threshold)])
 
-        return {"data": trace,
+        return {"data": traces,
                 "layout": go.Layout(title="Pandora ROC (selected data)",
                                     yaxis={"title": "Recall"}, xaxis={"title": "Error rate"},
                                     legend_orientation="h")}
