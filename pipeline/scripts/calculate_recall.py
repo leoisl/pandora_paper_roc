Speeding up the computation of the plot data by replacing gt_min, gt_max and step_gt by a fixed number of points in the ROC

pipeline/scripts/calculate_recall.py
@@ -24,9 +24,7 @@ recall_report_files_for_all_samples = (
 )
 output = Path(snakemake.output.recall_file_for_all_samples)
 
-min_gt = float(snakemake.wildcards.min_gt)
-step_gt = float(snakemake.wildcards.step_gt)
-max_gt = float(snakemake.wildcards.max_gt)
+number_of_points_in_ROC_curve = float(snakemake.params.number_of_points_in_ROC_curve)
 
 tool = snakemake.wildcards.tool
 coverage = snakemake.wildcards.coverage
@@ -42,7 +40,9 @@ recall_calculator = RecallCalculator.from_files(
 )
 
 
-max_gt = min(max_gt, recall_calculator.get_maximum_gt_conf())
+min_gt = recall_calculator.get_minimum_gt_conf()
+max_gt = recall_calculator.get_maximum_gt_conf()
+step_gt = (max_gt - min_gt) / number_of_points_in_ROC_curve
 logging.info(
     f"Calculating recall with min_gt = {min_gt}, step_gt = {step_gt}, and max_gt = {max_gt}"
 )
@@ -50,6 +50,8 @@ logging.info(
 gts = []
 recalls = []
 all_gts = list(np.arange(min_gt, max_gt, step_gt)) + [max_gt]
+assert(len(all_gts) == number_of_points_in_ROC_curve + 1)
+
 for gt in all_gts:
     try:
         recall = recall_calculator.calculate_recall(gt)
@@ -67,6 +69,7 @@ recall_df = pd.DataFrame(
         "strand_bias_threshold": [strand_bias_threshold] * len(gts),
         "gaps_threshold": [gaps_threshold] * len(gts),
         "GT": gts,
+        "step_GT": list(range(len(all_gts))),
         "recall": recalls
     }
 )