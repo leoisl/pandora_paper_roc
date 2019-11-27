Fixing an off by one error in the plots

pipeline/scripts/calculate_precision.py
@@ -50,7 +50,9 @@ logging.info(
 gts = []
 precisions = []
 error_rates = []
-all_gts = list(np.arange(min_gt, max_gt, step_gt)) + [max_gt]
+all_gts = list(np.arange(min_gt, max_gt, step_gt))
+if len(all_gts) == number_of_points_in_ROC_curve:
+    all_gts.append(max_gt)
 assert(len(all_gts) == number_of_points_in_ROC_curve + 1)
 
 for gt in all_gts: