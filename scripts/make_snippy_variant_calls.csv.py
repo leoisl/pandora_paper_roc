cd5f7efd5d572be173ab5c14172cf159fe60e280
leandro <leandro@ebi.ac.uk>
2019-11-28 10:21:46 +0000

    Updating make_snippy_variant_calls.csv.py to correct the snippy sample name

--- scripts/make_snippy_variant_calls.csv.py
+++ scripts/make_snippy_variant_calls.csv.py
@@ -1,10 +1,16 @@
 from collections import Counter
 from pathlib import Path
+import os
+from shutil import copyfile
+import subprocess
+
 
 config = {
     "testing": False,
     "input_folder": "/hps/nobackup/research/zi/rmcolq/paper_4_way/snippy_pick_refs/snippy",
-    "all_refs": ["063_STEC", "CFT073", "H131800734", "ST38"]
+    "output_folder": "new_snippy_dir/",
+    "all_refs": ["063_STEC", "CFT073", "H131800734", "ST38"],
+    "output_csv": "variant_calls.csv"
 }
 
 def get_dataset_name_from_vcf(refs, vcf):
@@ -28,19 +34,47 @@ def get_all_vcf_filenames_in_a_folder(folder):
     all_vcf_filenames = [filename.name for filename in all_vcf_filepaths]
     return sorted(all_vcf_filenames)
 
+def correct_snippy_sample_name(ref, vcf_ref_uncorrected, vcf_ref_corrected, vcf_uncorrected, vcf_corrected):
+    print(f"Correcting: {vcf_ref_uncorrected} -> {vcf_ref_corrected}")
+    print(f"Correcting: {vcf_uncorrected} -> {vcf_corrected}")
+
+    copyfile(vcf_ref_uncorrected, vcf_ref_corrected)
+
+    with open("sample_file", "w") as sample_file:
+        print(ref, file=sample_file)
+    subprocess.run(f"bcftools reheader -s sample_file -o {vcf_corrected} {vcf_uncorrected}", check=True, shell=True)
+
+
 
 def main():
     input_folder = config["input_folder"]
     all_refs = config["all_refs"]
+    output_csv_filepath = config["output_csv"]
+    output_folder = config["output_folder"]
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
 
+    print(f"Getting all datasets in {all_refs} ...")
     all_vcfs = get_all_vcf_filenames_in_a_folder(input_folder)
     all_datasets_in_all_refs = get_all_datasets_in_all_refs(all_vcfs, all_refs)
+    print(f"All datasets in {all_refs}: {all_datasets_in_all_refs}")
 
-    print("sample_id,tool,coverage,reference,vcf")
-    for dataset in all_datasets_in_all_refs:
-        for ref in all_refs:
-            print(f"{ref},snippy_{dataset},all,{input_folder}/snippy_{ref}_{dataset}.ref.fa,{input_folder}/snippy_{ref}_{dataset}.vcf")
+    print(f"Creating {output_csv_filepath} ...")
+    ref_and_vcfrefuncorrected_and_vcfrefcorrected_and_vcfuncorrected_and_vcfcorrected_list = []
+    with open(output_csv_filepath, "w") as output_csv_file:
+        for dataset in all_datasets_in_all_refs:
+            for ref in all_refs:
+                vcf_ref_uncorrected = f"{input_folder}/snippy_{ref}_{dataset}.ref.fa"
+                vcf_ref_corrected = f"{output_folder}/snippy_{ref}_{dataset}.ref.fa"
+                vcf_uncorrected = f"{input_folder}/snippy_{ref}_{dataset}.vcf"
+                vcf_corrected = f"{output_folder}/snippy_{ref}_{dataset}.vcf"
+                ref_and_vcfrefuncorrected_and_vcfrefcorrected_and_vcfuncorrected_and_vcfcorrected_list.append(
+                    (ref, vcf_ref_uncorrected, vcf_ref_corrected, vcf_uncorrected, vcf_corrected))
+                print(f"{ref},snippy_{dataset},all,{vcf_ref_corrected},{vcf_corrected}", file=output_csv_file)
+    print(f"Created {output_csv_filepath} !")
 
+    for ref, vcf_ref_uncorrected, vcf_ref_corrected, vcf_uncorrected, vcf_corrected in ref_and_vcfrefuncorrected_and_vcfrefcorrected_and_vcfuncorrected_and_vcfcorrected_list:
+        correct_snippy_sample_name(ref, vcf_ref_uncorrected, vcf_ref_corrected, vcf_uncorrected, vcf_corrected)
 
 
 
@@ -52,6 +86,7 @@ def main_test():
     test_get_dataset_name_from_vcf()
     test_get_all_datasets_in_all_refs()
     test_get_all_vcf_filenames_in_a_folder()
+    test_correct_snippy_sample_name()
 
 def test_get_dataset_name_from_vcf():
     all_refs = ["063_STEC", "CFT073", "H131800734", "ST38"]
@@ -84,8 +119,18 @@ def test_get_all_vcf_filenames_in_a_folder():
     print("test_get_all_vcf_filenames_in_a_folder(): ok")
 
 
-
-
+def test_correct_snippy_sample_name():
+    correct_snippy_sample_name("CFT073",
+                               "test_correct_sample_name_in_vcf/snippy_CFT073_CP_OK_HEADER.ref.fa",
+                               "test_correct_sample_name_in_vcf/snippy_CFT073_CP_OK_HEADER.ref.corrected.fa",
+                               "test_correct_sample_name_in_vcf/snippy_CFT073_CP_OK_HEADER.vcf",
+                               "test_correct_sample_name_in_vcf/snippy_CFT073_CP_OK_HEADER.corrected.vcf")
+    correct_snippy_sample_name("CFT073",
+                               "test_correct_sample_name_in_vcf/snippy_CFT073_CP_WRONG_HEADER.ref.fa",
+                               "test_correct_sample_name_in_vcf/snippy_CFT073_CP_WRONG_HEADER.ref.corrected.fa",
+                               "test_correct_sample_name_in_vcf/snippy_CFT073_CP_WRONG_HEADER.vcf",
+                               "test_correct_sample_name_in_vcf/snippy_CFT073_CP_WRONG_HEADER.corrected.vcf")
+    print("test_correct_snippy_sample_name(): ok")
 
 
 
