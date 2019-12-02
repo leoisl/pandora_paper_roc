8e9f342a16394cef205ecab51dc8d6b7ec54f2e1
leandro <leandro@ebi.ac.uk>
2019-12-02 00:21:37 +0000

    Updating coverage filter to use coverage instead of mean_coverage

--- tests/test_coverage_filter.py
+++ tests/test_coverage_filter.py
@@ -4,7 +4,7 @@ from evaluate.coverage_filter import CoverageFilter
 
 class TestCoverageFilter:
     def test_recordShouldBeFilteredOut_coverageBelowThresholdReturnsTrue(self):
-        record = MagicMock(mean_coverage=5)
+        record = MagicMock(coverage=5)
         coverage_filter = CoverageFilter(10.0)
 
         actual = coverage_filter.record_should_be_filtered_out(record)
@@ -13,7 +13,7 @@ class TestCoverageFilter:
         assert actual == expected
 
     def test_recordShouldBeFilteredOut_coverageAboveThresholdReturnsFalse(self):
-        record = MagicMock(mean_coverage=15)
+        record = MagicMock(coverage=15)
         coverage_filter = CoverageFilter(10.0)
 
         actual = coverage_filter.record_should_be_filtered_out(record)
@@ -22,7 +22,7 @@ class TestCoverageFilter:
         assert actual == expected
 
     def test_recordShouldBeFilteredOut_coverageEqualsThresholdReturnsFalse(self):
-        record = MagicMock(mean_coverage=10)
+        record = MagicMock(coverage=10)
         coverage_filter = CoverageFilter(10.0)
 
         actual = coverage_filter.record_should_be_filtered_out(record)
