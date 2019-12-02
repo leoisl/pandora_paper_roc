265ed1c02c2ffdf02559dfd257e1a8347efbc822
leandro <leandro@ebi.ac.uk>
2019-12-02 00:04:22 +0000

    VCFFactory now needs a VCF_creator_method to create a VCF (which can be Pandora or Snippy)

--- evaluate/vcf_file.py
+++ evaluate/vcf_file.py
@@ -1,17 +1,17 @@
 import pysam
 from typing import List, Dict
-from .vcf import VCF, BuggedVCFError, NullVCFError
+from .vcf import VCF, BuggedVCFError, NullVCFError, VCFFactory
 from collections import defaultdict
 
 
 class VCFFile:
-    def __init__(self, pysam_variant_file: pysam.VariantFile):
+    def __init__(self, pysam_variant_file: pysam.VariantFile, VCF_creator_method):
         self._sample_to_gene_to_VCFs = defaultdict(lambda: defaultdict(list))
         for variant_record in pysam_variant_file:
             for sample in variant_record.samples:
                 gene = variant_record.chrom
                 try:
-                    vcf = VCF.from_VariantRecord_and_Sample(variant_record, sample)
+                    vcf = VCF_creator_method(variant_record, sample)
                     self._sample_to_gene_to_VCFs[sample][gene].append(vcf)
                 except NullVCFError:
                     pass
