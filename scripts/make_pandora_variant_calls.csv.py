2e52d34df0e17cef48588b2752676f05864c267b
leandro <leandro@ebi.ac.uk>
2020-02-28 09:09:32 +0000

    Adding the vcf ref

--- scripts/make_pandora_variant_calls.csv.py
+++ scripts/make_pandora_variant_calls.csv.py
@@ -6,4 +6,8 @@ modes = ["no_denovo", "with_denovo"]
 base_path = "/hps/nobackup/research/zi/projects/pandora_paper_leandro/analysis"
 
 for sample, coverage, technology, mode in product(samples, coverages, technologies, modes):
-    print(",".join([sample, f"pandora_{technology}_{mode}", coverage, f"{base_path}/{technology}/{coverage}/random/compare_{mode}/pandora_multisample_genotyped.vcf"]))
+    print(",".join([sample,
+                    f"pandora_{technology}_{mode}",
+                    coverage,
+                    f"{base_path}/{technology}/{coverage}/random/compare_{mode}/pandora_multisample.vcf_ref.fa",
+                    f"{base_path}/{technology}/{coverage}/random/compare_{mode}/pandora_multisample_genotyped.vcf"]))
