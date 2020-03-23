37714d48074c5426f88a40d6c41b60d8bc590763
leandro <leandro@ebi.ac.uk>
2020-03-23 13:59:18 +0000

    Fixing the scripts to filter vcfs by gt_conf percentile

--- pipeline/scripts/filter_vcf_for_a_single_sample_by_gt_conf_percentile_for_snippy.sh
+++ pipeline/scripts/filter_vcf_for_a_single_sample_by_gt_conf_percentile_for_snippy.sh
@@ -2,16 +2,4 @@ gzipped_singlesample_vcf_file=$1
 gt_conf_percentile=$2
 singlesample_vcf_file_gt_conf_percentile_filtered=$3
 
-min_gt=`bcftools query -f'%QUAL\n' $gzipped_singlesample_vcf_file | sort -n | head -n 1`
-echo "min_gt = $min_gt"
-
-max_gt=`bcftools query -f'%QUAL\n' $gzipped_singlesample_vcf_file | sort -nr | head -n 1`
-echo "max_gt = $max_gt"
-
-expression=`echo "( ( $max_gt - $min_gt ) * $gt_conf_percentile * 0.01) + $min_gt"`
-echo "expression = $expression"
-
-gt_conf=`echo "$expression" | bc`
-echo "gt_conf = $gt_conf"
-
-bcftools view $gzipped_singlesample_vcf_file -i "QUAL>=${gt_conf}" > $singlesample_vcf_file_gt_conf_percentile_filtered
+bcftools view $gzipped_singlesample_vcf_file -i "QUAL>=${gt_conf_percentile}" > $singlesample_vcf_file_gt_conf_percentile_filtered
