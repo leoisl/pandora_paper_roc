60c8f861b9765593f15d493168b05c0372d11e71
leandro <leandro@ebi.ac.uk>
2020-03-30 16:10:23 +0100

    Adding snakemake pipeline to create masks

---
+++ external_scripts/build_masks/scripts/submit_lsf.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+JOB_NAME="snakemake_master_process."$(date --iso-8601='minutes')
+LOG_DIR=logs/
+MEMORY=2000
+PROFILE="lsf"
+
+mkdir -p $LOG_DIR
+
+bsub -R "select[mem>$MEMORY] rusage[mem=$MEMORY] span[hosts=1]" \
+    -M "$MEMORY" \
+    -o "$LOG_DIR"/"$JOB_NAME".o \
+    -e "$LOG_DIR"/"$JOB_NAME".e \
+    -J "$JOB_NAME" \
+      snakemake --profile "$PROFILE" --stats "$LOG_DIR"/snakemake_stats "$@"
+
+exit 0
