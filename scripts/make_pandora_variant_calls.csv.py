19eed0a10ed55fcf1e75a8e3e647942abfef121f
leandro <leandro@ebi.ac.uk>
2020-03-30 18:44:54 +0100

    Updating make_pandora_variant_calls.csv.py

--- scripts/make_pandora_variant_calls.csv.py
+++ scripts/make_pandora_variant_calls.csv.py
@@ -1,13 +1,14 @@
 from itertools import product
-samples = ["CFT073", "H131800734", "ST38", "063_STEC"]
-coverages = ["30x", "60x", "100x"]
-technologies = ["illumina", "nanopore"]
-modes = ["no_denovo", "with_denovo"]
-base_path = "/hps/nobackup/research/zi/projects/pandora_paper_leandro/analysis"
+samples = ["063_STEC", "CFT073", "Escherichia_coli_MINF_1A", "Escherichia_coli_MINF_1D", "Escherichia_coli_MINF_2E", "Escherichia_coli_MINF_7C", "Escherichia_coli_MINF_8D", "Escherichia_coli_MINF_9A", "Escherichia_coli_MSB1_1A", "Escherichia_coli_MSB1_3B", "Escherichia_coli_MSB1_3C", "Escherichia_coli_MSB1_3I", "Escherichia_coli_MSB1_4E", "Escherichia_coli_MSB1_4G", "Escherichia_coli_MSB1_4I", "Escherichia_coli_MSB1_6C", "Escherichia_coli_MSB1_6J", "Escherichia_coli_MSB1_7A", "Escherichia_coli_MSB1_7C", "Escherichia_coli_MSB1_8B", "Escherichia_coli_MSB1_8G", "Escherichia_coli_MSB1_9D", "Escherichia_coli_MSB1_9I", "Escherichia_coli_MSB2_1A", "H131800734", "ST38"]
+coverages = ["100x"]
+technologies = ["illumina"]
+modes = ["nodenovo", "withdenovo"]
+genotyping_modes = ["local", "global"]
+base_path = "/hps/nobackup/research/zi/projects/pandora_analysis_pipeline/analysis_26_way"
 
-for sample, coverage, technology, mode in product(samples, coverages, technologies, modes):
+for sample, coverage, technology, mode, genotyping_mode in product(samples, coverages, technologies, modes, genotyping_modes):
     print(",".join([sample,
-                    f"pandora_{technology}_{mode}",
+                    f"pandora_{technology}_{mode}_{genotyping_mode}_genotyping",
                     coverage,
-                    f"{base_path}/{technology}/{coverage}/random/compare_{mode}/pandora_multisample.vcf_ref.fa",
-                    f"{base_path}/{technology}/{coverage}/random/compare_{mode}/pandora_multisample_genotyped.vcf"]))
+                    f"{base_path}/{technology}/{coverage}/random/compare_{mode}_{genotyping_mode}_genotyping/pandora_multisample.vcf_ref.fa",
+                    f"{base_path}/{technology}/{coverage}/random/compare_{mode}_{genotyping_mode}_genotyping/pandora_multisample_genotyped_{genotyping_mode}.vcf"]))
