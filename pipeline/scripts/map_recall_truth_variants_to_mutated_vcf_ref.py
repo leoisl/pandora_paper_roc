230b59db97267858c0b7d818797122556cae5b91
leandro <leandro@ebi.ac.uk>
2020-04-01 10:37:10 +0100

    Somehow bwa indexes are becoming corrupted, doing several tries to check if this fixes

--- pipeline/scripts/map_recall_truth_variants_to_mutated_vcf_ref.py
+++ pipeline/scripts/map_recall_truth_variants_to_mutated_vcf_ref.py
@@ -14,10 +14,36 @@ logging.basicConfig(
     format="[%(asctime)s]:%(levelname)s: %(message)s",
     datefmt="%d/%m/%Y %I:%M:%S %p",
 )
-
-
-
 from evaluate.bwa import BWA
+
+
+def run_bwa_mem(query, ref):
+    nb_tries = 10
+    success = False
+    for _ in range(nb_tries):
+        try:
+            logging.info(f"Mapping {query} to {ref}")
+            logging.info(f"bwa index {ref}")
+            subprocess.check_call(f"bwa index {ref}", shell=True)
+            time.sleep(60.0)  # sleeps 60 seconds due to cluster file latency
+
+            logging.info(f"bwa mem {ref} {query}")
+            BWA.map_query_to_ref(
+                query=query,
+                ref=ref,
+                output=output,
+                threads=threads,
+            )
+
+            # all good
+            success = True
+            break
+        except subprocess.CalledProcessError as error:
+            last_error = error
+            logging.info("CalledProcessError captured, retrying...")
+
+        if not success:
+            raise last_error
 
 
 # setup
@@ -29,18 +55,6 @@ threads = int(snakemake.threads)
 
 # API usage
 for ref, output in zip(refs, outputs):
-    logging.info(f"Mapping {query} to {ref}")
-    logging.info(f"bwa index {ref}")
-    subprocess.check_call(f"bwa index {ref}", shell=True)
-    time.sleep(10.0)  # sleeps 10 seconds due to cluster file latency
-
-    logging.info(f"bwa mem {ref} {query}")
-    BWA.map_query_to_ref(
-        query=query,
-        ref=ref,
-        output=output,
-        threads=threads,
-    )
-
+    run_bwa_mem(query, ref)
 
 logging.info(f"Done")
