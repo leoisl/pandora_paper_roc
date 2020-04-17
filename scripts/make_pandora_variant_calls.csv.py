66787272ccdffc6937a2689cb120ba8468fee60b
leandro <leandro@ebi.ac.uk>
2020-04-17 14:08:10 +0100

    Updating scripts to make the input csvs for the pipeline

--- scripts/make_pandora_variant_calls.csv.py
+++ scripts/make_pandora_variant_calls.csv.py
@@ -1,10 +1,10 @@
 from itertools import product
-samples = ["063_STEC", "CFT073", "Escherichia_coli_MINF_1A", "Escherichia_coli_MINF_1D", "Escherichia_coli_MINF_2E", "Escherichia_coli_MINF_7C", "Escherichia_coli_MINF_8D", "Escherichia_coli_MINF_9A", "Escherichia_coli_MSB1_1A", "Escherichia_coli_MSB1_3B", "Escherichia_coli_MSB1_3C", "Escherichia_coli_MSB1_3I", "Escherichia_coli_MSB1_4E", "Escherichia_coli_MSB1_4G", "Escherichia_coli_MSB1_4I", "Escherichia_coli_MSB1_6C", "Escherichia_coli_MSB1_6J", "Escherichia_coli_MSB1_7A", "Escherichia_coli_MSB1_7C", "Escherichia_coli_MSB1_8B", "Escherichia_coli_MSB1_8G", "Escherichia_coli_MSB1_9D", "Escherichia_coli_MSB1_9I", "Escherichia_coli_MSB2_1A", "H131800734", "ST38"]
+samples = ["063_STEC", "CFT073", "Escherichia_coli_MINF_1A", "Escherichia_coli_MINF_1D", "Escherichia_coli_MINF_2E", "Escherichia_coli_MINF_7C", "Escherichia_coli_MINF_8D", "Escherichia_coli_MINF_9A", "Escherichia_coli_MSB1_1A", "Escherichia_coli_MSB1_3B", "Escherichia_coli_MSB1_3I", "Escherichia_coli_MSB1_4E", "Escherichia_coli_MSB1_4I", "Escherichia_coli_MSB1_6C", "Escherichia_coli_MSB1_6J", "Escherichia_coli_MSB1_7A", "Escherichia_coli_MSB1_7C", "Escherichia_coli_MSB1_8B", "Escherichia_coli_MSB1_8G", "Escherichia_coli_MSB1_9D", "Escherichia_coli_MSB1_9I", "Escherichia_coli_MSB2_1A", "H131800734", "ST38"]
 coverages = ["100x"]
 technologies = ["illumina"]
 modes = ["nodenovo", "withdenovo"]
-genotyping_modes = ["local", "global"]
-base_path = "/hps/nobackup/research/zi/projects/pandora_analysis_pipeline/analysis_26_way"
+genotyping_modes = ["global"]
+base_path = "/hps/nobackup/iqbal/leandro/pandora_analysis_pipeline/analysis_24_way"
 
 print("sample_id,tool,coverage,reference,vcf")
 for sample, coverage, technology, mode, genotyping_mode in product(samples, coverages, technologies, modes, genotyping_modes):
