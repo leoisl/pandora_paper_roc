0c9b0c78deb58bccd159e615da8cb28eb1ce749b
leandro <leandro@ebi.ac.uk>
2020-04-22 09:51:33 +0100

    WIP: recall deduplication

--- evaluate/deduplicate_pairwise_snps.py
+++ evaluate/deduplicate_pairwise_snps.py
@@ -3,8 +3,10 @@ from evaluate.mummer import ShowSNPsDataframe
 from pathlib import Path
 import re
 import pickle
-from typing import Tuple
+from typing import Tuple, Iterable, Set, List, Generator
 import functools
+import itertools
+
 
 class NotASNP(Exception):
     pass
@@ -78,12 +80,53 @@ class PairwiseVariation:
                self.allele_2 == other.allele_1 or self.allele_2 == other.allele_2
 
 
+# Note: trivial class, not tested
+class PangenomeVariation:
+    def __init__(self, alleles: Iterable[Allele]):
+        self._alleles = set(alleles)
+    @property
+    def alleles(self) -> Set[Allele]:
+        return self._alleles
+
+    def __eq__(self, other: object):
+        if isinstance(other, PangenomeVariation):
+            return self.alleles==other.alleles
+        else:
+            return False
+
+
+
+# Note: trivial class, not tested
+class PangenomeVariations:
+    def __init__(self):
+        self._pangenome_variations = []
+    @property
+    def pangenome_variations(self) -> List[PangenomeVariation]:
+        return self._pangenome_variations
+
+    def append(self, pangenome_variation: PangenomeVariation):
+        self.pangenome_variations.append(pangenome_variation)
+
+    def __eq__(self, other: object):
+        if isinstance(other, PangenomeVariations):
+            return self.pangenome_variations==other.pangenome_variations
+        else:
+            return False
+
+
 class DeduplicationGraph:
     def __init__(self):
         self._graph = nx.Graph()
     @property
-    def graph(self) -> nx.Graph:
+    def graph(self):
         return self._graph
+    @property
+    def nodes(self):
+        return self.graph.nodes
+    @property
+    def edges(self):
+        return self.graph.edges
+
 
     @staticmethod
     def _get_ref_and_query_from_ShowSNPsDataframe_filepath(ShowSNPsDataframe_filepath: str) -> Tuple[str, str]:
@@ -94,15 +137,18 @@ class DeduplicationGraph:
         query = matches.group(2)
         return ref, query
 
+
     # Note: not tested (trivial method)
     @staticmethod
     def _load_pickled_ShowSNPsDataframe(df_filepath: str) -> ShowSNPsDataframe:
         with open(df_filepath, "rb") as df_fh:
             return pickle.load(df_fh)
 
+
     # Note: not tested (trivial method)
     def _add_pairwise_variation(self, pairwise_variation: PairwiseVariation) -> None:
-        self.graph.add_node(pairwise_variation)
+        self._graph.add_node(pairwise_variation)
+
 
     def _add_variants_from_ShowSNPsDataframe_core(self, ref: str, query: str, snps_df: ShowSNPsDataframe) -> None:
         for ref_chrom, ref_pos, ref_sub, query_chrom, query_pos, query_sub in \
@@ -119,7 +165,45 @@ class DeduplicationGraph:
 
 
     # Note: not tested (trivial method)
-    def add_variants_from_ShowSNPsDataframe(self, ShowSNPsDataframe_filepath: str) -> None:
+    def add_variants_from_ShowSNPsDataframe_filepath(self, ShowSNPsDataframe_filepath: str) -> None:
         ref, query = DeduplicationGraph._get_ref_and_query_from_ShowSNPsDataframe_filepath(ShowSNPsDataframe_filepath)
         snps_df = DeduplicationGraph._load_pickled_ShowSNPsDataframe(ShowSNPsDataframe_filepath)
         self._add_variants_from_ShowSNPsDataframe_core(ref, query, snps_df)
+
+
+    def add_edge(self, variant_1, variant_2) -> None:
+        self._graph.add_edge(variant_1, variant_2)
+
+
+    def build_edges(self) -> None:
+        """
+        Note: Should be called after all nodes are added.
+        """
+        #TODO: the runtime is quadratic on the number of variants. In the 24-way will be too much. We will have 24*23/2=276
+        #TODO: pairwise comparisons. These comparisons have around 100k variants, so the code inside this for will be executed
+        #TODO: (276 * 100000) ** 2 = ~761 trillion times... probably the bottleneck will be here.
+        #TODO: implement an indexing of variants per (genome, chrom, position/10000)
+        #TODO: a variant only needs to check the buckets belonging to its alleles
+        for variant_1, variant_2 in itertools.product(self.nodes, self.nodes):
+            if variant_1 != variant_2:
+                if variant_1.share_allele(variant_2):
+                    self.add_edge(variant_1, variant_2)
+
+
+    # Note: not tested (trivial method)
+    def _get_connected_components(self) -> Generator[Set[PairwiseVariation], None, None]:
+        return nx.connected_components(self.graph)
+    
+    def get_pangenome_variations(self) -> PangenomeVariations:
+        pangenome_variations = PangenomeVariations()
+
+        connected_components = self._get_connected_components()
+        for connected_component_index, connected_component in enumerate(connected_components):
+            alleles_in_connected_component = []
+            for pairwise_variation in connected_component:
+                alleles_in_connected_component.append(pairwise_variation.allele_1)
+                alleles_in_connected_component.append(pairwise_variation.allele_2)
+            pangenome_variation = PangenomeVariation(alleles_in_connected_component)
+            pangenome_variations.append(pangenome_variation)
+
+        return pangenome_variations
--- tests/test_deduplicate_pairwise_snps.py
+++ tests/test_deduplicate_pairwise_snps.py
@@ -1,25 +1,28 @@
-from evaluate.deduplicate_pairwise_snps import NotASNP, Allele, PairwiseVariation, DeduplicationGraph
+from evaluate.deduplicate_pairwise_snps import NotASNP, Allele, PairwiseVariation, DeduplicationGraph, PangenomeVariation, PangenomeVariations
 import pandas as pd
-import pytest
 from io import StringIO
+from unittest.mock import patch, PropertyMock
+from unittest import TestCase
+import numpy as np
+from collections import defaultdict
 
-class TestAllele:
+class TestAllele(TestCase):
     def test_constructor_isSNP_constructorOK(self):
         Allele("genome_1", "chrom_1", 10, "A")
-        assert True
+        self.assertTrue(True)
 
     def test_constructor_isNotASNP_constructorRaisesNotASNP(self):
-        with pytest.raises(NotASNP):
+        with self.assertRaises(NotASNP):
             Allele("genome_1", "chrom_1", 10, "AC")
 
     def test_constructor_isDeletion_constructorRaisesNotASNP(self):
-        with pytest.raises(NotASNP):
+        with self.assertRaises(NotASNP):
             Allele("genome_1", "chrom_1", 10, "")
 
     def test_hash_equalAlleles_returnsEqualHashValues(self):
         allele_1 = Allele("genome_1", "chrom_1", 10, "A")
         allele_2 = Allele("genome_1", "chrom_1", 10, "A")
-        assert hash(allele_1) == hash(allele_2)
+        self.assertEqual(hash(allele_1), hash(allele_2))
 
     def test_hash_differentGenomes_returnsDifferentHashValues(self):
         allele_1 = Allele("genome_1", "chrom_1", 10, "A")
@@ -107,7 +110,7 @@ class TestAllele:
         assert (allele_1 >= allele_2) == True
 
 
-class TestPairwiseVariation:
+class TestPairwiseVariation(TestCase):
     def test___constructor___ordered_alleles(self):
         allele_1 = Allele("genome_1", "chrom_1", 10, "A")
         allele_2 = Allele("genome_2", "chrom_2", 20, "A")
@@ -177,7 +180,12 @@ class TestPairwiseVariation:
         assert pairwise_variation_1.share_allele(pairwise_variation_2) == True
 
 
-class TestDeduplicationGraph:
+class TestDeduplicationGraph(TestCase):
+    def setUp(self):
+        self.alleles = [Allele(f"genome_{i}", "chrom_1", 1, "A") for i in range(10)]
+        self.pairwise_mutations = [PairwiseVariation(self.alleles[i*2], self.alleles[i*2+1]) for i in range(5)]
+        self.deduplication_graph = DeduplicationGraph()
+
     def test___get_ref_and_query_from_ShowSNPsDataframe_filepath___absolute_path(self):
         filepath = "/home/leandro/git/pandora1_paper/analysis_output/recall/snps_dfs/CFT073/CFT073_and_H131800734.snps_df.pickle"
         ref, query = DeduplicationGraph._get_ref_and_query_from_ShowSNPsDataframe_filepath(filepath)
@@ -199,31 +207,28 @@ class TestDeduplicationGraph:
         assert ref == "A" and query=="B"
 
     def test___add_variants_from_ShowSNPsDataframe_core___empty_df___no_variations_added(self):
-        deduplication_graph = DeduplicationGraph()
         snps_df = pd.read_csv(StringIO(
 """ref_pos,ref_sub,query_sub,query_pos,nearest_mismatch,nearest_end,ref_len,query_len,ref_context,query_context,ref_strand,query_strand,ref_chrom,query_chrom
 """
         ))
-        deduplication_graph._add_variants_from_ShowSNPsDataframe_core("genome_1", "genome_2", snps_df)
-        graph_is_empty = len(deduplication_graph.graph)==0
+        self.deduplication_graph._add_variants_from_ShowSNPsDataframe_core("genome_1", "genome_2", snps_df)
+        graph_is_empty = self.deduplication_graph.graph.number_of_nodes()==0
         assert graph_is_empty
 
     def test___add_variants_from_ShowSNPsDataframe_core___one_variation_in_df___one_variation_added(self):
-        deduplication_graph = DeduplicationGraph()
         snps_df = pd.read_csv(StringIO(
 """ref_pos,ref_sub,query_sub,query_pos,nearest_mismatch,nearest_end,ref_len,query_len,ref_context,query_context,ref_strand,query_strand,ref_chrom,query_chrom
 1,A,C,2,0,0,0,0,ACGT,ACGT,1,1,chrom_1,chrom_2
 """
         ))
-        deduplication_graph._add_variants_from_ShowSNPsDataframe_core("genome_1", "genome_2", snps_df)
+        self.deduplication_graph._add_variants_from_ShowSNPsDataframe_core("genome_1", "genome_2", snps_df)
 
         pairwise_variation = PairwiseVariation(Allele("genome_1", "chrom_1", 1, "A"),
                                                Allele("genome_2", "chrom_2", 2, "C"))
-        assert len(deduplication_graph.graph)==1 and pairwise_variation in deduplication_graph.graph
+        assert self.deduplication_graph.graph.number_of_nodes()==1 and self.deduplication_graph.graph.has_node(pairwise_variation)
 
 
     def test___add_variants_from_ShowSNPsDataframe_core___one_SNP_variation_four_non_SNP_variation_in_df___only_SNP_variation_added(self):
-        deduplication_graph = DeduplicationGraph()
         snps_df = pd.read_csv(StringIO(
 """ref_pos,ref_sub,query_sub,query_pos,nearest_mismatch,nearest_end,ref_len,query_len,ref_context,query_context,ref_strand,query_strand,ref_chrom,query_chrom
 1,,C,2,0,0,0,0,ACGT,ACGT,1,1,chrom_1,chrom_2
@@ -233,16 +238,15 @@ class TestDeduplicationGraph:
 1,AA,C,2,0,0,0,0,ACGT,ACGT,1,1,chrom_1,chrom_2
 """
         ), na_filter=False)
-        deduplication_graph._add_variants_from_ShowSNPsDataframe_core("genome_1", "genome_2", snps_df)
+        self.deduplication_graph._add_variants_from_ShowSNPsDataframe_core("genome_1", "genome_2", snps_df)
 
         pairwise_variation = PairwiseVariation(Allele("genome_1", "chrom_1", 1, "A"),
                                                Allele("genome_2", "chrom_2", 2, "C"))
-        assert len(deduplication_graph.graph)==1 and pairwise_variation in deduplication_graph.graph
+        assert self.deduplication_graph.graph.number_of_nodes()==1 and self.deduplication_graph.graph.has_node(pairwise_variation)
 
 
 
     def test___add_variants_from_ShowSNPsDataframe_core___three_SNP_variation_four_non_SNP_variation_in_df___only_SNPs_variations_added(self):
-        deduplication_graph = DeduplicationGraph()
         snps_df = pd.read_csv(StringIO(
 """ref_pos,ref_sub,query_sub,query_pos,nearest_mismatch,nearest_end,ref_len,query_len,ref_context,query_context,ref_strand,query_strand,ref_chrom,query_chrom
 10,G,T,20,0,0,0,0,ACGT,ACGT,1,1,chrom_10,chrom_20
@@ -255,7 +259,7 @@ class TestDeduplicationGraph:
 100,A,T,200,0,0,0,0,ACGT,ACGT,1,1,chrom_100,chrom_200
 """
         ), na_filter=False)
-        deduplication_graph._add_variants_from_ShowSNPsDataframe_core("genome_1", "genome_2", snps_df)
+        self.deduplication_graph._add_variants_from_ShowSNPsDataframe_core("genome_1", "genome_2", snps_df)
 
 
         pairwise_variation_1 = PairwiseVariation(Allele("genome_1", "chrom_10", 10, "G"),
@@ -264,7 +268,150 @@ class TestDeduplicationGraph:
                                                  Allele("genome_2", "chrom_2", 2, "C"))
         pairwise_variation_3 = PairwiseVariation(Allele("genome_1", "chrom_100", 100, "A"),
                                                  Allele("genome_2", "chrom_200", 200, "T"))
-        assert len(deduplication_graph.graph)==3 and \
-               pairwise_variation_1 in deduplication_graph.graph and \
-               pairwise_variation_2 in deduplication_graph.graph and \
-               pairwise_variation_3 in deduplication_graph.graph
+        assert self.deduplication_graph.graph.number_of_nodes() == 3 and \
+               self.deduplication_graph.graph.has_node(pairwise_variation_1) and \
+               self.deduplication_graph.graph.has_node(pairwise_variation_2) and \
+               self.deduplication_graph.graph.has_node(pairwise_variation_3)
+
+
+    def test___add_variants_from_ShowSNPsDataframe_core___one_same_variation_in_three_dfs___one_variation_added(self):
+        snps_df = pd.read_csv(StringIO(
+"""ref_pos,ref_sub,query_sub,query_pos,nearest_mismatch,nearest_end,ref_len,query_len,ref_context,query_context,ref_strand,query_strand,ref_chrom,query_chrom
+1,A,C,2,0,0,0,0,ACGT,ACGT,1,1,chrom_1,chrom_2
+"""
+        ))
+        self.deduplication_graph._add_variants_from_ShowSNPsDataframe_core("genome_1", "genome_2", snps_df)
+        self.deduplication_graph._add_variants_from_ShowSNPsDataframe_core("genome_1", "genome_2", snps_df)
+        self.deduplication_graph._add_variants_from_ShowSNPsDataframe_core("genome_1", "genome_2", snps_df)
+
+        pairwise_variation = PairwiseVariation(Allele("genome_1", "chrom_1", 1, "A"),
+                                               Allele("genome_2", "chrom_2", 2, "C"))
+        assert self.deduplication_graph.graph.number_of_nodes()==1 and self.deduplication_graph.graph.has_node(pairwise_variation)
+
+    def test___add_variants_from_ShowSNPsDataframe_core___one_variation_in_three_dfs___three_variations_added(self):
+        snps_df = pd.read_csv(StringIO(
+"""ref_pos,ref_sub,query_sub,query_pos,nearest_mismatch,nearest_end,ref_len,query_len,ref_context,query_context,ref_strand,query_strand,ref_chrom,query_chrom
+1,A,C,2,0,0,0,0,ACGT,ACGT,1,1,chrom_1,chrom_2
+"""
+        ))
+        self.deduplication_graph._add_variants_from_ShowSNPsDataframe_core("genome_1", "genome_2", snps_df)
+        self.deduplication_graph._add_variants_from_ShowSNPsDataframe_core("genome_3", "genome_2", snps_df)
+        self.deduplication_graph._add_variants_from_ShowSNPsDataframe_core("genome_4", "genome_2", snps_df)
+
+        pairwise_variation_1 = PairwiseVariation(Allele("genome_1", "chrom_1", 1, "A"),
+                                                 Allele("genome_2", "chrom_2", 2, "C"))
+        pairwise_variation_2 = PairwiseVariation(Allele("genome_3", "chrom_1", 1, "A"),
+                                                 Allele("genome_2", "chrom_2", 2, "C"))
+        pairwise_variation_3 = PairwiseVariation(Allele("genome_4", "chrom_1", 1, "A"),
+                                                 Allele("genome_2", "chrom_2", 2, "C"))
+        assert self.deduplication_graph.graph.number_of_nodes()==3 and \
+               self.deduplication_graph.graph.has_node(pairwise_variation_1) and \
+               self.deduplication_graph.graph.has_node(pairwise_variation_2) and \
+               self.deduplication_graph.graph.has_node(pairwise_variation_3)
+
+    @patch.object(PairwiseVariation, PairwiseVariation.share_allele.__name__, return_value=False)
+    @patch.object(DeduplicationGraph, "nodes", new_callable=PropertyMock)
+    def test___build_edges___no_shared_alleles___no_edge_built(self, nodes_mock, share_allele_mock):
+        nodes_mock.return_value = self.pairwise_mutations
+        self.deduplication_graph.build_edges()
+        self.assertEqual(self.deduplication_graph.graph.number_of_edges(), 0)
+
+    @patch.object(PairwiseVariation, PairwiseVariation.share_allele.__name__, autospec=True)
+    @patch.object(DeduplicationGraph, "nodes", new_callable=PropertyMock)
+    def test___build_edges___four_shared_alleles___four_edges_built(self, nodes_mock, share_allele_mock):
+        # set up
+        nodes_mock.return_value = self.pairwise_mutations
+        # create a random matrix with 3 variations with shared alleles
+        allele_sharing_matrix = defaultdict(lambda: defaultdict(int))
+        allele_sharing_matrix[self.pairwise_mutations[0]][self.pairwise_mutations[2]] = 1  # add both ways
+        allele_sharing_matrix[self.pairwise_mutations[2]][self.pairwise_mutations[0]] = 1  # add both ways
+        allele_sharing_matrix[self.pairwise_mutations[1]][self.pairwise_mutations[0]] = 1  # add one way
+        allele_sharing_matrix[self.pairwise_mutations[3]][self.pairwise_mutations[3]] = 1  # self loop, not added
+        allele_sharing_matrix[self.pairwise_mutations[4]][self.pairwise_mutations[2]] = 1  # add one way
+        def share_allele_return_mock(variant_1, variant_2):
+            return allele_sharing_matrix[variant_1][variant_2]
+        share_allele_mock.side_effect = share_allele_return_mock
+
+        self.deduplication_graph.build_edges()
+        self.assertTrue(self.deduplication_graph.graph.has_edge(self.pairwise_mutations[0], self.pairwise_mutations[2]))
+        self.assertTrue(self.deduplication_graph.graph.has_edge(self.pairwise_mutations[2], self.pairwise_mutations[0]))
+        self.assertTrue(self.deduplication_graph.graph.has_edge(self.pairwise_mutations[1], self.pairwise_mutations[0]))
+        self.assertTrue(self.deduplication_graph.graph.has_edge(self.pairwise_mutations[0], self.pairwise_mutations[1]))
+        self.assertTrue(self.deduplication_graph.graph.has_edge(self.pairwise_mutations[4], self.pairwise_mutations[2]))
+        self.assertTrue(self.deduplication_graph.graph.has_edge(self.pairwise_mutations[2], self.pairwise_mutations[4]))
+        self.assertEqual(self.deduplication_graph.graph.number_of_edges(), 3)
+
+
+    @patch.object(DeduplicationGraph, DeduplicationGraph._get_connected_components.__name__)
+    def test___get_pangenome_variations___totally_unconnected_graph(self, get_connected_components_mock):
+        get_connected_components_mock.return_value = [
+            {self.pairwise_mutations[0]},
+            {self.pairwise_mutations[1]},
+            {self.pairwise_mutations[2]},
+            {self.pairwise_mutations[3]},
+            {self.pairwise_mutations[4]}
+        ]
+        pangenome_variations_actual = self.deduplication_graph.get_pangenome_variations()
+
+        pangenome_variations_expected = PangenomeVariations()
+        for i in range(5):
+            pangenome_variations_expected.append(PangenomeVariation([
+                self.pairwise_mutations[i].allele_1, self.pairwise_mutations[i].allele_2]))
+
+        self.assertEqual(pangenome_variations_actual, pangenome_variations_expected)
+
+
+    @patch.object(DeduplicationGraph, DeduplicationGraph._get_connected_components.__name__)
+    def test___get_pangenome_variations___totally_connected_graph(self, get_connected_components_mock):
+        get_connected_components_mock.return_value = [
+            {self.pairwise_mutations[0], self.pairwise_mutations[1], self.pairwise_mutations[2],
+             self.pairwise_mutations[3], self.pairwise_mutations[4]}
+        ]
+        pangenome_variations_actual = self.deduplication_graph.get_pangenome_variations()
+
+        pangenome_variations_expected = PangenomeVariations()
+        pangenome_variations_expected.append(PangenomeVariation(self.alleles))
+
+        self.assertEqual(pangenome_variations_actual, pangenome_variations_expected)
+
+    @patch.object(DeduplicationGraph, DeduplicationGraph._get_connected_components.__name__)
+    def test___get_pangenome_variations___isolated_node_and_a_path(self, get_connected_components_mock):
+        get_connected_components_mock.return_value = [
+            {self.pairwise_mutations[0], self.pairwise_mutations[1], self.pairwise_mutations[3], self.pairwise_mutations[4]},
+            {self.pairwise_mutations[2]}
+        ]
+        pangenome_variations_actual = self.deduplication_graph.get_pangenome_variations()
+
+        pangenome_variations_expected = PangenomeVariations()
+        pangenome_variations_expected.append(PangenomeVariation([
+            self.pairwise_mutations[0].allele_1, self.pairwise_mutations[0].allele_2,
+            self.pairwise_mutations[1].allele_1, self.pairwise_mutations[1].allele_2,
+            self.pairwise_mutations[3].allele_1, self.pairwise_mutations[3].allele_2,
+            self.pairwise_mutations[4].allele_1, self.pairwise_mutations[4].allele_2,
+        ]))
+        pangenome_variations_expected.append(PangenomeVariation([
+            self.pairwise_mutations[2].allele_1, self.pairwise_mutations[2].allele_2,
+        ]))
+
+        self.assertEqual(pangenome_variations_actual, pangenome_variations_expected)
+
+    @patch.object(DeduplicationGraph, DeduplicationGraph._get_connected_components.__name__)
+    def test___get_pangenome_variations___two_and_three_sized_components(self, get_connected_components_mock):
+        get_connected_components_mock.return_value = [
+            {self.pairwise_mutations[0], self.pairwise_mutations[1], self.pairwise_mutations[3]},
+            {self.pairwise_mutations[2], self.pairwise_mutations[4]}
+        ]
+        pangenome_variations_actual = self.deduplication_graph.get_pangenome_variations()
+
+        pangenome_variations_expected = PangenomeVariations()
+        pangenome_variations_expected.append(PangenomeVariation([
+            self.pairwise_mutations[0].allele_1, self.pairwise_mutations[0].allele_2,
+            self.pairwise_mutations[1].allele_1, self.pairwise_mutations[1].allele_2,
+            self.pairwise_mutations[3].allele_1, self.pairwise_mutations[3].allele_2,
+        ]))
+        pangenome_variations_expected.append(PangenomeVariation([
+            self.pairwise_mutations[2].allele_1, self.pairwise_mutations[2].allele_2,
+            self.pairwise_mutations[4].allele_1, self.pairwise_mutations[4].allele_2,
+        ]))
+
+        self.assertEqual(pangenome_variations_actual, pangenome_variations_expected)
