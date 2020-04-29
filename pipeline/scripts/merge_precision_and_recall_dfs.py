b8abf1c6812dd52a90a633a180f30a2858191a71
leandro <leandro@ebi.ac.uk>
2020-04-29 21:33:51 +0100

    Fixing the merge scripts to output the new recall data

--- pipeline/scripts/merge_precision_and_recall_dfs.py
+++ pipeline/scripts/merge_precision_and_recall_dfs.py
@@ -13,7 +13,10 @@ recall_df = pd.read_csv(recall_file, sep="\t")
 error_rate_and_recall_df = pd.merge(precision_df, recall_df,
                     on=["tool", "coverage", "coverage_threshold", "strand_bias_threshold", "gaps_threshold", "step_GT"])[
     ["tool", "coverage", "coverage_threshold", "strand_bias_threshold", "gaps_threshold", "step_GT", "error_rate",
-     "nb_of_correct_calls", "nb_of_total_calls", "recall", "nb_of_truth_probes_found", "nb_of_truth_probes_in_total"]
+     "nb_of_correct_calls", "nb_of_total_calls",
+     "recalls_wrt_truth_probes", "nbs_of_truth_probes_found", "nbs_of_truth_probes_in_total",
+     "recalls_wrt_variants_where_all_allele_seqs_were_found", "recalls_wrt_variants_found_wrt_alleles",
+     "nbs_variants_where_all_allele_seqs_were_found", "nbs_variants_found_wrt_alleles", "nbs_variants_total"]
 ]
 
 # output
