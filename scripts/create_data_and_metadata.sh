b93d9a241a4d293d2c07c2ffa9e6dd1c1381726d
leandro <leandro@ebi.ac.uk>
2020-09-10 23:24:53 +0100

    Removing unused script

--- scripts/create_data_and_metadata.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-wget https://www.dropbox.com/s/7804udh9c6th7v9/data_and_metadata.zip?dl=1 -O data_and_metadata.zip
-cd .. && unzip scripts/data_and_metadata.zip
-rm scripts/data_and_metadata.zip
