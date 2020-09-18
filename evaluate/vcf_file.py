7cbc387d229f34a253dc1e879beabd4d6bde499d
leandro <leandro@ebi.ac.uk>
2020-09-18 18:37:39 +0100

    Adding capabilities for VCF file to write itself

--- evaluate/vcf_file.py
+++ evaluate/vcf_file.py
@@ -1,11 +1,12 @@
 import pysam
-from typing import List, Dict
+from typing import List, Dict, TextIO
 from .vcf import VCF, NullVCFError, VCFFactory
 from collections import defaultdict
 
 
 class VCFFile:
     def __init__(self, pysam_variant_file: pysam.VariantFile, VCF_creator_method):
+        self._header = pysam_variant_file.header
         self._sample_to_gene_to_VCFs = defaultdict(lambda: defaultdict(list))
         for variant_record in pysam_variant_file:
             for sample in variant_record.samples:
@@ -20,7 +21,18 @@ class VCFFile:
     def sample_to_gene_to_VCFs(self) -> Dict[str, Dict[str, List[VCF]]]:
         return self._sample_to_gene_to_VCFs
 
+    @property
+    def header(self) -> str:
+        return str(self._header)
+
     def get_VCF_records_given_sample_and_gene(
         self, sample: str, gene_name: str
     ) -> List[VCF]:
         return self.sample_to_gene_to_VCFs[sample][gene_name]
+
+    def write(self, filehandler: TextIO):
+        filehandler.write(self.header)
+        for (sample, gene_to_VCFs_of_a_sample) in self.sample_to_gene_to_VCFs.items():
+            for gene, vcfs in gene_to_VCFs_of_a_sample.items():
+                for vcf in vcfs:
+                    filehandler.write(str(vcf))
--- tests/test_vcf_file.py
+++ tests/test_vcf_file.py
@@ -2,6 +2,8 @@ from unittest.mock import patch, Mock, PropertyMock
 from evaluate.vcf_file import VCFFile
 from evaluate.vcf import NullVCFError, VCFFactory
 import pytest
+import pysam
+from io import StringIO
 
 
 @pytest.fixture
@@ -59,9 +61,21 @@ def chrom_1_raises_NullVCFError_others_are_fine(pysam_variant_record, sample):
         return vcf_record_2_mock
 
 
+class pysam_VariantRecord_Mock:
+    def __init__(self, list_of_records, header=""):
+        self.list_of_records=list_of_records
+        self.header=header
+
+    def __iter__(self):
+        return self.list_of_records.__iter__()
+
+    def __next__(self):
+        return self.list_of_records.__next__()
+
+
 class Test_VCFFile:
     def test___constructor___no_records_in_VCF_returns_nothing(self):
-        vcf_file = VCFFile([], VCFFactory.create_Pandora_VCF_from_VariantRecord_and_Sample)
+        vcf_file = VCFFile(pysam_VariantRecord_Mock([]), VCFFactory.create_Pandora_VCF_from_VariantRecord_and_Sample)
         actual = vcf_file.sample_to_gene_to_VCFs
 
         expected = {}
@@ -70,7 +84,7 @@ class Test_VCFFile:
     @patch.object(VCFFactory, VCFFactory.create_Pandora_VCF_from_VariantRecord_and_Sample.__name__, return_value=vcf_record_1_mock)
     def test___constructor___one_record_in_one_sample_and_one_gene(self, from_VariantRecord_and_Sample_Mock,
                                                                    pysam_variant_record_mock_that_maps_to_chrom_1_and_one_sample):
-        vcf_file = VCFFile([pysam_variant_record_mock_that_maps_to_chrom_1_and_one_sample], VCFFactory.create_Pandora_VCF_from_VariantRecord_and_Sample)
+        vcf_file = VCFFile(pysam_VariantRecord_Mock([pysam_variant_record_mock_that_maps_to_chrom_1_and_one_sample]), VCFFactory.create_Pandora_VCF_from_VariantRecord_and_Sample)
         actual = vcf_file.sample_to_gene_to_VCFs
 
         expected = {"sample_1": {"chrom_1": [vcf_record_1_mock]}}
@@ -80,7 +94,7 @@ class Test_VCFFile:
                   return_value=vcf_record_1_mock)
     def test___constructor___one_record_in_two_samples_and_one_gene(self, from_VariantRecord_and_Sample_Mock,
                                                                     pysam_variant_record_mock_that_maps_to_chrom_1_and_two_samples):
-        vcf_file = VCFFile([pysam_variant_record_mock_that_maps_to_chrom_1_and_two_samples], VCFFactory.create_Pandora_VCF_from_VariantRecord_and_Sample)
+        vcf_file = VCFFile(pysam_VariantRecord_Mock([pysam_variant_record_mock_that_maps_to_chrom_1_and_two_samples]), VCFFactory.create_Pandora_VCF_from_VariantRecord_and_Sample)
         actual = vcf_file.sample_to_gene_to_VCFs
 
         expected = {"sample_1": {"chrom_1": [vcf_record_1_mock]},
@@ -92,8 +106,8 @@ class Test_VCFFile:
     def test___constructor___two_records_in_one_sample_and_two_genes(self, from_VariantRecord_and_Sample_Mock,
                                                                      pysam_variant_record_mock_that_maps_to_chrom_1_and_one_sample,
                                                                      pysam_variant_record_mock_that_maps_to_chrom_2_and_one_sample):
-        vcf_file = VCFFile([pysam_variant_record_mock_that_maps_to_chrom_1_and_one_sample,
-                            pysam_variant_record_mock_that_maps_to_chrom_2_and_one_sample], VCFFactory.create_Pandora_VCF_from_VariantRecord_and_Sample)
+        vcf_file = VCFFile(pysam_VariantRecord_Mock([pysam_variant_record_mock_that_maps_to_chrom_1_and_one_sample,
+                            pysam_variant_record_mock_that_maps_to_chrom_2_and_one_sample]), VCFFactory.create_Pandora_VCF_from_VariantRecord_and_Sample)
         actual = vcf_file.sample_to_gene_to_VCFs
 
         expected = {"sample_1": {"chrom_1": [vcf_record_1_mock], "chrom_2": [vcf_record_2_mock]}}
@@ -103,8 +117,8 @@ class Test_VCFFile:
                   side_effect=[vcf_record_1_mock, vcf_record_2_mock])
     def test___constructor___two_records_in_one_sample_and_one_gene(self, from_VariantRecord_and_Sample_Mock,
                                                                     pysam_variant_record_mock_that_maps_to_chrom_1_and_one_sample):
-        vcf_file = VCFFile([pysam_variant_record_mock_that_maps_to_chrom_1_and_one_sample,
-                            pysam_variant_record_mock_that_maps_to_chrom_1_and_one_sample], VCFFactory.create_Pandora_VCF_from_VariantRecord_and_Sample)
+        vcf_file = VCFFile(pysam_VariantRecord_Mock([pysam_variant_record_mock_that_maps_to_chrom_1_and_one_sample,
+                            pysam_variant_record_mock_that_maps_to_chrom_1_and_one_sample]), VCFFactory.create_Pandora_VCF_from_VariantRecord_and_Sample)
         actual = vcf_file.sample_to_gene_to_VCFs
 
         expected = {"sample_1": {"chrom_1": [vcf_record_1_mock, vcf_record_2_mock]}}
@@ -115,8 +129,8 @@ class Test_VCFFile:
     def test___constructor___two_records_in_one_sample_and_two_genes___first_is_null_and_is_not_added(self, from_VariantRecord_and_Sample_Mock,
                                                                      pysam_variant_record_mock_that_maps_to_chrom_1_and_one_sample,
                                                                      pysam_variant_record_mock_that_maps_to_chrom_2_and_one_sample):
-        vcf_file = VCFFile([pysam_variant_record_mock_that_maps_to_chrom_1_and_one_sample,
-                            pysam_variant_record_mock_that_maps_to_chrom_2_and_one_sample], VCFFactory.create_Pandora_VCF_from_VariantRecord_and_Sample)
+        vcf_file = VCFFile(pysam_VariantRecord_Mock([pysam_variant_record_mock_that_maps_to_chrom_1_and_one_sample,
+                            pysam_variant_record_mock_that_maps_to_chrom_2_and_one_sample]), VCFFactory.create_Pandora_VCF_from_VariantRecord_and_Sample)
         actual = vcf_file.sample_to_gene_to_VCFs
 
         expected = {"sample_1": {"chrom_2": [vcf_record_2_mock]}}
@@ -128,8 +142,8 @@ class Test_VCFFile:
     def test___constructor___two_records_in_one_sample_and_two_genes___second_is_null_and_is_not_added(self, from_VariantRecord_and_Sample_Mock,
                                                                      pysam_variant_record_mock_that_maps_to_chrom_1_and_one_sample,
                                                                      pysam_variant_record_mock_that_maps_to_chrom_2_and_one_sample):
-        vcf_file = VCFFile([pysam_variant_record_mock_that_maps_to_chrom_2_and_one_sample,
-                            pysam_variant_record_mock_that_maps_to_chrom_1_and_one_sample,], VCFFactory.create_Pandora_VCF_from_VariantRecord_and_Sample)
+        vcf_file = VCFFile(pysam_VariantRecord_Mock([pysam_variant_record_mock_that_maps_to_chrom_2_and_one_sample,
+                            pysam_variant_record_mock_that_maps_to_chrom_1_and_one_sample,]), VCFFactory.create_Pandora_VCF_from_VariantRecord_and_Sample)
         actual = vcf_file.sample_to_gene_to_VCFs
 
         expected = {"sample_1": {"chrom_2": [vcf_record_2_mock]}}
@@ -172,11 +186,11 @@ class Test_VCFFile:
                           side_effect=[vcf_record_1_mock, vcf_record_2_mock, vcf_record_3_mock,
                                vcf_record_4_mock, vcf_record_5_mock, vcf_record_6_mock,
                                vcf_record_7_mock, vcf_record_8_mock]):
-            vcf_file = VCFFile([pysam_variant_record_mock_that_maps_to_chrom_1_and_sample_1,
+            vcf_file = VCFFile(pysam_VariantRecord_Mock([pysam_variant_record_mock_that_maps_to_chrom_1_and_sample_1,
                                 pysam_variant_record_mock_that_maps_to_chrom_1_and_sample_1_2_3,
                                 pysam_variant_record_mock_that_maps_to_chrom_2_and_sample_1_2,
                                 pysam_variant_record_mock_that_maps_to_chrom_2_and_sample_2,
-                                another_pysam_variant_record_mock_that_maps_to_chrom_2_and_sample_2], VCFFactory.create_Pandora_VCF_from_VariantRecord_and_Sample)
+                                another_pysam_variant_record_mock_that_maps_to_chrom_2_and_sample_2]), VCFFactory.create_Pandora_VCF_from_VariantRecord_and_Sample)
             actual = vcf_file.sample_to_gene_to_VCFs
 
             expected = {"sample_1": {"chrom_1": [vcf_record_1_mock, vcf_record_2_mock],
@@ -188,28 +202,70 @@ class Test_VCFFile:
 
     @patch.object(VCFFile, "sample_to_gene_to_VCFs", new_callable=PropertyMock, return_value = sample_with_some_genes)
     def test___get_VCF_records_given_sample_and_gene___sample_1_gene_1(self, *mocks):
-        vcf_file = VCFFile([], VCFFactory.create_Pandora_VCF_from_VariantRecord_and_Sample)
+        vcf_file = VCFFile(pysam_VariantRecord_Mock([]), VCFFactory.create_Pandora_VCF_from_VariantRecord_and_Sample)
         actual = vcf_file.get_VCF_records_given_sample_and_gene("sample_1", "gene_1")
         expected = [1,2,3,4]
         assert actual == expected
 
     @patch.object(VCFFile, "sample_to_gene_to_VCFs", new_callable=PropertyMock, return_value=sample_with_some_genes)
     def test___get_VCF_records_given_sample_and_gene___sample_1_gene_2(self, *mocks):
-        vcf_file = VCFFile([], VCFFactory.create_Pandora_VCF_from_VariantRecord_and_Sample)
+        vcf_file = VCFFile(pysam_VariantRecord_Mock([]), VCFFactory.create_Pandora_VCF_from_VariantRecord_and_Sample)
         actual = vcf_file.get_VCF_records_given_sample_and_gene("sample_1", "gene_2")
         expected = [5,6]
         assert actual == expected
 
     @patch.object(VCFFile, "sample_to_gene_to_VCFs", new_callable=PropertyMock, return_value = sample_with_some_genes)
     def test___get_VCF_records_given_sample_and_gene___sample_2_gene_1(self, *mocks):
-        vcf_file = VCFFile([], VCFFactory.create_Pandora_VCF_from_VariantRecord_and_Sample)
+        vcf_file = VCFFile(pysam_VariantRecord_Mock([]), VCFFactory.create_Pandora_VCF_from_VariantRecord_and_Sample)
         actual = vcf_file.get_VCF_records_given_sample_and_gene("sample_2", "gene_1")
         expected = [7, 8, 9]
         assert actual == expected
 
     @patch.object(VCFFile, "sample_to_gene_to_VCFs", new_callable=PropertyMock, return_value=sample_with_some_genes)
     def test___get_VCF_records_given_sample_and_gene___sample_2_gene_2(self, *mocks):
-        vcf_file = VCFFile([], VCFFactory.create_Pandora_VCF_from_VariantRecord_and_Sample)
+        vcf_file = VCFFile(pysam_VariantRecord_Mock([]), VCFFactory.create_Pandora_VCF_from_VariantRecord_and_Sample)
         actual = vcf_file.get_VCF_records_given_sample_and_gene("sample_2", "gene_2")
         expected = [10]
         assert actual == expected
+
+
+    def test___write(self):
+        vcf_filepath = "tests/test_cases/test.vcf"
+        with pysam.VariantFile(vcf_filepath) as pysam_variant_file:
+            vcf_file = VCFFile(pysam_variant_file=pysam_variant_file, VCF_creator_method=VCFFactory.create_Pandora_VCF_from_VariantRecord_and_Sample)
+
+        filehandler = StringIO()
+        vcf_file.write(filehandler)
+        actual_vcf = filehandler.getvalue()
+        filehandler.close()
+
+        expected_vcf="""##fileformat=VCFv4.3
+##FILTER=<ID=PASS,Description="All filters passed">
+##fileDate==26/04/19
+##ALT=<ID=SNP,Description="SNP">
+##ALT=<ID=PH_SNPs,Description="Phased SNPs">
+##ALT=<ID=INDEL,Description="Insertion-deletion">
+##ALT=<ID=COMPLEX,Description="Complex variant, collection of SNPs and indels">
+##INFO=<ID=SVTYPE,Number=1,Type=String,Description="Type of variant">
+##ALT=<ID=SIMPLE,Description="Graph bubble is simple">
+##ALT=<ID=NESTED,Description="Variation site was a nested feature in the graph">
+##ALT=<ID=TOO_MANY_ALTS,Description="Variation site was a multinested feature with too many alts to include all in the VCF">
+##INFO=<ID=GRAPHTYPE,Number=1,Type=String,Description="Type of graph feature">
+##contig=<ID=GC00000001_155>
+##FORMAT=<ID=GT,Number=1,Type=String,Description="Dummy">
+##FORMAT=<ID=MEAN_FWD_COVG,Number=1,Type=String,Description="Dummy">
+##FORMAT=<ID=MEAN_REV_COVG,Number=1,Type=String,Description="Dummy">
+##FORMAT=<ID=MED_FWD_COVG,Number=1,Type=String,Description="Dummy">
+##FORMAT=<ID=MED_REV_COVG,Number=1,Type=String,Description="Dummy">
+##FORMAT=<ID=SUM_FWD_COVG,Number=1,Type=String,Description="Dummy">
+##FORMAT=<ID=SUM_REV_COVG,Number=1,Type=String,Description="Dummy">
+##FORMAT=<ID=GAPS,Number=1,Type=String,Description="Dummy">
+##FORMAT=<ID=LIKELIHOOD,Number=1,Type=String,Description="Dummy">
+##FORMAT=<ID=GT_CONF,Number=1,Type=String,Description="Dummy">
+#CHROM	POS	ID	REF	ALT	QUAL	FILTER	INFO	FORMAT	sample
+GC00000001_155	1	.	CTGCCCGTTGGC	TTGGGGGAAGGCTCTGCACTGCCCGTTGGC,TTGGGGGAAGGCTCTGCACTGCCTGTTGGT	.	.	SVTYPE=COMPLEX;GRAPHTYPE=NESTED	GT:MEAN_FWD_COVG:MEAN_REV_COVG:MED_FWD_COVG:MED_REV_COVG:SUM_FWD_COVG:SUM_REV_COVG:GAPS:LIKELIHOOD:GT_CONF	1:24,6,0:30,7,0:24,0,0:30,0,0:24,24,0:30,30,0:0,0.75,1:-63.3221,-326.079,-432.546:262.757
+GC00000001_155	1	.	ACGT	TTGGGGGAAGGCTCTGCACTGCCCGTTGGC,TTGGGGGAAGGCTCTGCACTGCCTGTTGGT	.	.	SVTYPE=COMPLEX;GRAPHTYPE=NESTED	GT:MEAN_FWD_COVG:MEAN_REV_COVG:MED_FWD_COVG:MED_REV_COVG:SUM_FWD_COVG:SUM_REV_COVG:GAPS:LIKELIHOOD:GT_CONF	1:6,24,0:7,30,0:0,24,0:0,30,0:24,24,0:30,30,0:0.75,0,1:-326.079,-63.3221,-432.546:262.757
+GC00000001_155	1	.	CTGCCCGTTGGC	TTGGGGGAAGGCTCTGCACTGCCCGTTGGC,TTGGGGGAAGGCTCTGCACTGCCTGTTGGT	.	.	SVTYPE=COMPLEX;GRAPHTYPE=NESTED	GT:MEAN_FWD_COVG:MEAN_REV_COVG:MED_FWD_COVG:MED_REV_COVG:SUM_FWD_COVG:SUM_REV_COVG:GAPS:LIKELIHOOD:GT_CONF	0:24,6,0:30,7,0:24,0,0:30,0,0:24,24,0:30,30,0:0,0.75,1:-63.3221,-326.079,-432.546:262.757
+"""
+
+        assert actual_vcf == expected_vcf
