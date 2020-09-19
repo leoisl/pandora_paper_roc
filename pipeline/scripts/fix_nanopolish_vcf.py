8972497514a896a939f35907ecff4d7881dd62c2
leandro <leandro@ebi.ac.uk>
2020-09-19 22:10:07 +0100

    Now sorting and removing duplicated lines in nanopolish VCF

--- pipeline/scripts/fix_nanopolish_vcf.py
+++ pipeline/scripts/fix_nanopolish_vcf.py
@@ -7,9 +7,26 @@ else:
     from pipeline.scripts.fix_snippy_vcf import FixSnippyVCF
 
 
-# FixNanopolishVCF is just like FixSnippyVCF
 class FixNanopolishVCF(FixSnippyVCF):
-    pass
+    def process_vcf(self, original_vcf, corrected_vcf, sample):
+        with open(original_vcf) as original_vcf_filehandler,\
+             open(corrected_vcf, "w") as corrected_vcf_filehandler:
+            headers, records = self.get_header_and_record_lines(original_vcf_filehandler)
+            corrected_headers = self.correct_headers(headers, sample)
+            print("\n".join(corrected_headers), file=corrected_vcf_filehandler)
+
+            if len(records) > 0:
+                corrected_records = self.correct_records(records)
+
+                # remove repeated records
+                corrected_records = list(set(corrected_records))
+
+                # sort records by chrom and pos
+                corrected_records = sorted(corrected_records,
+                                           key=lambda record: (record.split()[0], int(record.split()[1])))
+
+                print("\n".join(corrected_records), file=corrected_vcf_filehandler)
+
 
 if __name__=="__main__":
     # setup
