f8ca91513348c5d144d55c565d8a0ad29e15cf5c
leandro <leandro@ebi.ac.uk>
2020-10-02 15:29:17 +0100

    Adding tools to the plot title in recall_per_ref_per_nb_of_samples_per_clade

--- pipeline/scripts/make_recall_per_ref_per_nb_of_samples_per_clade_plot.py
+++ pipeline/scripts/make_recall_per_ref_per_nb_of_samples_per_clade_plot.py
@@ -17,11 +17,12 @@ csv_data = snakemake.input.csv_data
 plots = snakemake.output.plots
 gif = snakemake.output.gif
 list_with_number_of_samples = snakemake.params.list_with_number_of_samples
+tools=snakemake.wildcards.tools_to_keep
 
 
 # call R to generate the several plots
 for input_csv, output_plot, nb_of_samples in zip(csv_data, plots, list_with_number_of_samples):
-    args = f"{input_csv} 0.0 recall_number_of_samples={nb_of_samples} {output_plot}"
+    args = f"{input_csv} 0.0 recall_{tools}_number_of_samples={nb_of_samples} {output_plot}"
     shell(f"Rscript eda/recall_per_ref_per_nb_of_samples_per_clade/clade_plots.R {args}")
 
 # generate the gif
