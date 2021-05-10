8d27f0710ba40e2ce9bdf28677b87ec64c30b1c5
Leandro Ishi <leandro.ishi.lima@gmail.com>
2021-05-10 14:33:20 +0100

    Cleaning up repo (pandora FN exploration)

--- external_scripts/pandora_FN_exploration/get_data.sh
+++
@@ -1,5 +0,0 @@
-mkdir -p data
-cd data
-rsync -v -r --relative codon-ext:/hps/nobackup/iqbal/leandro/pdrv/out_20_way/pandora_gene_distance/gene_distance_pandora_paper_tag1/genes_from_truth_or_ref .
-rsync -v -r --relative codon-ext:/hps/nobackup/iqbal/leandro/pdrv/out_20_way/pandora_paper_roc/analysis_output_pandora_paper_tag1/recall/reports/*/100x/pandora_nanopore_withdenovo/coverage_filter_0/strand_bias_filter_0.0/gaps_filter_1.0/gt_conf_percentile_0 .
-cd ..
